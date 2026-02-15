009795f9c4ea150243732333a94ccd59c4d7b3e1
em828kentacuk <em828@kent.ac.uk>
2026-02-15 11:51:17 +0000

    added the options list to fleet_manager2

--- fleet_manager2.py
+++ fleet_manager2.py
@@ -2,3 +2,26 @@ name = ["Spock", "Picard", "Riker", "Data", "Worf"]
 rank = ["Captain", "Captain", "Commander", "Lt. Commander", "Lieutenant"]
 division = ["Command", "Command", "Command", "Operations", "Security"]
 id = [1, 2, 3, 4, 5]
+
+
+active = True
+
+def main():
+
+    while True:
+
+        print("\n--- MENU ---")
+        print("1. Display Roster")        
+        print("2. Add Crew")
+        print("3. Remove Crew")
+        print("4. Update Rank")
+        print("5. Search Crew")
+        print("6. Filter by Division")
+        print("7. Calculate Payroll")
+        print("8. Count officers by Commander or Captain ranks")
+        option = input("Select option: ")
+
+        
+        
+        
+        main()
