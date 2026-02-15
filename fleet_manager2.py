4950f64e4b782cc2e1f1285321f9fc095c0234ed
em828kentacuk <em828@kent.ac.uk>
2026-02-15 11:48:49 +0000

    adding strings to fleet manager

---
+++ fleet_manager2.py
@@ -0,0 +1,4 @@
+name = ["Spock", "Picard", "Riker", "Data", "Worf"]
+rank = ["Captain", "Captain", "Commander", "Lt. Commander", "Lieutenant"]
+division = ["Command", "Command", "Command", "Operations", "Security"]
+id = [1, 2, 3, 4, 5]
